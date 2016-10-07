702c3cab9fd16d01752c7c105b65eeca66cb49e8
Stephen Clay <stephen.clay@hotmail.com>
2016-10-07 13:37:37 +0100

    Initial Commit

---
+++ src/WebApplication8/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnaClayChildminding.Models
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public String Availability { get; set; }
+    }
+}
---
+++ src/WebApplication8/Models/Testimonial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UnaClayChildminding.Models
+{
+    public class Testimonial
+    {
+        public int ID { get; set; }
+        public String Description { get; set; }
+    }
+}

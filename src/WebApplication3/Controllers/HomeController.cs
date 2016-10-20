bb1376c01f4a8159ceb62dee6af50ed71baa913b
Stephen Clay <sclay@datix.co.uk>
2016-10-20 10:28:37 +0100

    completed rewrite in dotnet core

---
+++ src/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnaClayChildminding.Models;
+
+namespace UnaClayChildminding.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Qualifications()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Testimonials()
+        {
+            return View();
+            //return View(db.Testimonials.ToList());
+        }
+        [HttpGet]
+        public IActionResult Admin()
+        {
+            var a = new Admin() { Availability = "3 Full time places" };
+            return View(a);
+        }
+
+        [HttpPost]
+        public IActionResult Admin(Admin a)
+        {
+            return View(a);
+        }
+
+
+        public IActionResult Parents()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WebApplication3/models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnaClayChildminding.Models
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public String Availability { get; set; }
+    }
+}
